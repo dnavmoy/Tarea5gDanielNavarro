362df939ce33318e3ec6780fb659ca82bf4e9703
dnavmoy <dnavmoy119@g.educaand.es>
2024-02-05 08:24:51 +0100

    parte c

--- src/main/java/paqueteC/MainC.java
+++ src/main/java/paqueteC/MainC.java
@@ -17,6 +17,7 @@ public class MainC {
     
     public static void main(String[] args) {
         
+        //commit
         ListadoVaquerias listado = new ListadoVaquerias();
         
         listado.getListado().put(1, new VaqueriaOrdenada());
